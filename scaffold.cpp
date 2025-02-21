3f433dc4d6dfb287386c085ed10b5565fa62d270
Annette Jing <annette.jing@gmail.com>
2025-02-21 15:51:31 -0800

    Update calculator to use math functions

--- scaffold.cpp
+++ scaffold.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
-#include randfuncs.h
+#include "randfuncs.h"
+#include "mathfuncs.h"
 
 using namespace std;
 
@@ -14,10 +15,18 @@ int main(int argc, char *argv[]) {
 
         if ("help" == command)
         {
-            // to do
-	    cout << "There are three rand functions with the following jobs: 1) flips coin, 
-                2) rolls six-sided dice, 3) rolls twenty-sided dice. Enter in coin, six dice,
-                or twenty dice, respectively, to use these functions." << endl;
+	    cout << "---Help documentation---
+        RANDOM
+        There are three rand functions with the following jobs: 
+        1) flips coin, 
+        2) rolls six-sided dice, 
+        3) rolls twenty-sided dice. 
+        Enter in coin, six dice, or twenty dice, respectively, to use these functions.
+        MATH FUNCTIONS
+        Enter '-' to subtract numbers;
+        Enter '*' to multiply numbers;
+        Enter '/' to divide numbers;
+        " << endl;
         }
         else if (command == "coin") {
             return FlipCoins();
@@ -28,11 +37,28 @@ int main(int argc, char *argv[]) {
         else if (command == "twenty dice") {
             return RollTwentyDice();
         }
+        else if ("-" == command)
+        {
+            cout << "Enter two numbers: ";
+            cin >> a >> b;
+            cout << "Result: " << subtract(a, b) << endl;
+        }
+        else if ("*" == command)
+        {
+            cout << "Enter two numbers: ";
+            cin >> a >> b;
+            cout << "Result: " << multiply(a, b) << endl;
+        }
+        else if ("/" == command)
+        {
+            cout << "Enter two numbers: ";
+            cin >> a >> b;
+            cout << "Result: " << divide(a, b) << endl;
+        }
         else
         {
-           // to do for start
-	   cout << "unknown command" << endl;
-	   return 1;
+            cout << "unknown command" << endl;
+            return 1;
         }
     } while (EXIT != command);
     return 0;
