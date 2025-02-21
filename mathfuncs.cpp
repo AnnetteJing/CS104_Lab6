1b44824fd33cca7bb10624538e353427df2fd125
Annette Jing <annette.jing@gmail.com>
2025-02-21 15:38:59 -0800

    Implement math functions

---
+++ mathfuncs.cpp
@@ -0,0 +1,14 @@
+#include "mathfuncs.h"
+
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    return a / b;
+}
---
+++ mathfuncs.h
@@ -0,0 +1,7 @@
+#ifndef MATH_OPERATIONS_H
+#define MATH_OPERATIONS_H
+
+double subtract(double a, double b);
+double divide(double a, double b);
+
+#endif
